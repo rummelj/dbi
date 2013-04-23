887d4c6adb5a6d8f21da75192e9979bf6d8c23a1
johannes <johannes@localhost>
2013-04-23 15:59:05 +0200

    First commit

---
+++ DBI/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: johannes
+ *
+ * Created on April 23, 2013, 3:57 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
